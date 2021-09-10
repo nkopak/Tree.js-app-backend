364f96d6bc528b5798b973f685da9b5a7dffb2b8
nkopak <123nazik@gmail.com>
2021-09-10 15:54:29 +0300

    atlas connect

--- src/service/stateService.js
+++ src/service/stateService.js
@@ -1,10 +1,49 @@
 const State = require("../database/models/State")
+const { MongoClient } = require("mongodb")
+require("dotenv").config()
+
+const uri = process.env.MONGODB_ATLAS_URI
+const client = new MongoClient(uri)
+
+client.connect()
+const database = client.db(process.env.CLIENT_DB)
+const state = database.collection(process.env.MONGODB_COLLECTION)
 
 module.exports = {
-  getStateById: id => State.findOne({ id }),
+  getStateById: async id => {
+    try {
+      await client.connect()
 
-  saveState: stateObject => State.create(stateObject),
+      const stateById = await state.findOne({ id })
 
-  updateStateById: (id, stateObject) =>
-    State.updateOne({ id }, { $set: stateObject })
+      return stateById
+    } finally {
+      await client.close()
+    }
+  },
+
+  saveState: async (id, stateObject) => {
+    try {
+      await client.connect()
+
+      await state.insertOne(stateObject)
+      const result = await state.findOne({ id })
+
+      return result
+    } finally {
+      await client.close()
+    }
+  },
+
+  updateStateById: async (id, stateObject) => {
+    try {
+      await client.connect()
+
+      const result = await state.updateOne({ id }, { $set: stateObject })
+
+      return result
+    } finally {
+      await client.close()
+    }
+  }
 }
